fedbba967197e4252d418073a90fcfa52ee7f517
Drake Gooding <dgooding1008@gmail.com>
2021-07-12 17:59:45 -0500

    Updates to all the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tags, {
+  through: {
+    model: ProductTags,
+    unique: false
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  through: {
+    model: ProductTags,
+    unique: false
+  }
+});
 
 module.exports = {
   Product,
